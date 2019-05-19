1821c79c077cf54a89fb144d401cebb06b6b779e
tpiwonski <tpiwonski@gmail.com>
2019-05-19 22:35:29 +0200

    Fixes in translation

--- lang/dictionary/views/translate_entry.py
+++ lang/dictionary/views/translate_entry.py
@@ -1,15 +1,13 @@
 from django import forms
 from django.shortcuts import redirect
 
-from lang.common.component import ComponentView
 from lang.dictionary.controllers import TranslateEntry, AddEntries
-from lang.dictionary.models import Entry
 from lang.dictionary.views.base import PageView
 from lang.dictionary.views.current_date import CurrentDateView
 
 
-class TranslateForm(forms.Form):
-    text = forms.CharField(max_length=255)
+class TranslateEntryForm(forms.Form):
+    q = forms.CharField(max_length=255)
 
 
 class TranslateEntryView(PageView):
@@ -20,27 +18,27 @@ class TranslateEntryView(PageView):
     translate_entry_controller = TranslateEntry()
 
     def get(self, request, *args, **kwargs):
-        context = {
-            'translate_form': TranslateForm()
-        }
-        return self.render(context, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        translate_form = TranslateForm(request.POST)
+        translate_form = TranslateEntryForm(request.GET)
         context = {
             'translate_form': translate_form
         }
         if not translate_form.is_valid():
             return self.render(context, **kwargs)
-        
-        entries = self.translate_entry_controller.execute(**translate_form.cleaned_data)
-        context.update({
-            'entries': entries
-        })
+
+        text = translate_form.cleaned_data['q']
+        if text:
+            entries = self.translate_entry_controller.execute(text)
+            context.update({
+                'entries': entries
+            })
+
         return self.render(context, **kwargs)
 
 
 class AddEntryTranslationsView(PageView):
+    fragment_id = 'translate-entry'
+    fragment_template = 'dictionary/fragments/entry_list.html'
+    component_classes = [CurrentDateView]
     add_entries_controller = AddEntries()
     
     def post(self, request, *args, **kwargs):
@@ -62,30 +60,8 @@ class AddEntryTranslationsView(PageView):
                 if translations:
                     entries.append({'text': entry['text'], 'language': entry['language'], 'translations': translations})
 
-        self.add_entries_controller.execute(entries)
-        return redirect('all-entries')
-
-
-class DeleteEntriesForm(forms.Form):
-
-    def __init__(self, entries, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
-
-        for entry in entries:
-            self.fields['entry_{}'.format(entry['id'])] = forms.BooleanField()
-            for translation in entry['translations']:
-                self.fields['translation_{}'.format(translation['id'])] = forms.BooleanField()
-
-
-class DeleteEntriesView(ComponentView):
-    fragment_template = 'dictionary/fragments/entry_deleted.html'
-    entry_repository = Entry.objects
-
-    def post(self, request, *args, **kwargs):
-        for name, value in request.POST.items():
-            if name.startswith('entry_'):
-                self.entry_repository.delete_entry_with_translations(value)
-            elif name.startswith('translation_'):
-                self.entry_repository.delete_entry(value)
-
-        return self.render_fragment({}, **kwargs)
+        entries = self.add_entries_controller.execute(entries)
+        context = {
+            'entries': entries
+        }
+        return self.render(context, **kwargs)
