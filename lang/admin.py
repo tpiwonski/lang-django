f6a08811a47cc4041d5756ad57f49e616224b810
tpiwonski <tpiwonski@gmail.com>
2019-08-20 23:01:45 +0200

    Add admin

--- lang/admin.py
+++ lang/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from lang.dictionary.models import Entry, Relation, RelationExample, Example
+
+admin.site.register(Entry)
+admin.site.register(Relation)
+admin.site.register(RelationExample)
+admin.site.register(Example)
--- langsite/settings/base.py
+++ langsite/settings/base.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
+    # 'corsheaders',
     'widget_tweaks',
     'lang'
 ]
@@ -135,6 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGGING = {
     'version': 1,
