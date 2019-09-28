e275be7a6e8c10c1bfc4c49861a76189973594e7
tpiwo <tpiwonski@gmail.com>
2019-09-28 23:57:14 +0200

    Use data classes

--- lang/dictionary/controllers/add_entries.py
+++ lang/dictionary/controllers/add_entries.py
@@ -1,9 +1,11 @@
 import itertools
+from typing import List
 
 from django.db import transaction
 
 from lang.dictionary.db.entry import ENTRY_TYPE_SENTENCE
 from lang.dictionary.models import Entry, Example, Recording
+from lang.dictionary.types import EntryData
 
 
 class AddEntries(object):
@@ -12,23 +14,25 @@ class AddEntries(object):
     recording_repository = Recording.objects
 
     @transaction.atomic
-    def execute(self, results):
+    def execute(self, entries_data: List[EntryData]):
         entries = []
-        for entry_data in results:
-            entry = self.entry_repository.get_by_text(entry_data['text'], entry_data['language'], entry_data['type'])
+        for entry_data in entries_data:
+            entry = self.entry_repository.get_by_text(entry_data.text, entry_data.language, entry_data.type)
             if not entry:
-                entry = Entry.create(entry_data['text'], entry_data['language'], entry_data['type'])
+                entry = Entry.create(entry_data.text, entry_data.language, entry_data.type)
 
-            for entry_recording_data in entry_data['recordings']:
-                if not entry.has_recording(entry_recording_data['url']):
-                    entry.add_recording(entry_recording_data['url'])
+            for entry_recording_data in entry_data.recordings:
+                if not entry.has_recording(entry_recording_data.url):
+                    entry.add_recording(entry_recording_data.url)
 
-            for translation_data in entry_data['translations']:
+            for translation_data in entry_data.translations:
                 synonyms = []
-                for translation_entry in translation_data['entries']:
-                    translation = self.entry_repository.get_by_text(translation_entry['text'], translation_data['language'], translation_data['type'])
+                for translation_entry in translation_data.entries:
+                    translation = self.entry_repository.get_by_text(
+                        translation_entry.text, translation_data.language, translation_data.type)
                     if not translation:
-                        translation = Entry.create(translation_entry['text'], translation_data['language'], translation_data['type'])
+                        translation = Entry.create(
+                            translation_entry.text, translation_data.language, translation_data.type)
 
                     synonyms.append(translation)
 
@@ -36,24 +40,28 @@ class AddEntries(object):
                     if not relation:
                         relation = entry.add_translation(translation)
 
-                    for entry_recording_data in translation_data['recordings']:
-                        if not translation.has_recording(entry_recording_data['url']):
-                            translation.add_recording(entry_recording_data['url'])
+                    for entry_recording_data in translation_data.recordings:
+                        if not translation.has_recording(entry_recording_data.url):
+                            translation.add_recording(entry_recording_data.url)
 
-                    for example_data in translation_data.get('examples', []):
-                        example = self.example_repository.get_by_text(example_data['text'])
+                    for example_data in translation_data.examples:
+                        example = self.example_repository.get_by_text(example_data.text)
                         if not example:
 
-                            example_entry = self.entry_repository.get_by_text(example_data['text'], entry_data['language'], ENTRY_TYPE_SENTENCE)
+                            example_entry = self.entry_repository.get_by_text(
+                                example_data.text, entry_data.language, ENTRY_TYPE_SENTENCE)
                             if not example_entry:
-                                example_entry = Entry.create(example_data['text'], entry_data['language'], ENTRY_TYPE_SENTENCE)
+                                example_entry = Entry.create(
+                                    example_data.text, entry_data.language, ENTRY_TYPE_SENTENCE)
 
-                            example_translation = self.entry_repository.get_by_text(example_data['translation'], translation_data['language'], ENTRY_TYPE_SENTENCE)
+                            example_translation = self.entry_repository.get_by_text(
+                                example_data.translation, translation_data.language, ENTRY_TYPE_SENTENCE)
                             if not example_translation:
-                                example_translation = Entry.create(example_data['translation'], translation_data['language'], ENTRY_TYPE_SENTENCE)
+                                example_translation = Entry.create(
+                                    example_data.translation, translation_data.language, ENTRY_TYPE_SENTENCE)
 
-                            if not example_translation.has_recording(example_data['recording']['url']):
-                                example_translation.add_recording(example_data['recording']['url'])
+                            if not example_translation.has_recording(example_data.recording.url):
+                                example_translation.add_recording(example_data.recording.url)
 
                             if not example_entry.get_translation(example_translation):
                                 example_entry.add_translation(example_translation)
